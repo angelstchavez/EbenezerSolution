ce577e76047d0222d5d5bc474a4982f98e23975e
angelstchavez <angelstchavez@gmail.com>
2024-01-25 15:09:43 -0500

    feat: add test inserts

---
+++ EbenezerSolution.Insert.sql
@@ -0,0 +1,327 @@
+﻿USE EbenezerSolution;
+GO
+
+-- Insert Person 1
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Ana Martinez', 'Femenino', '3101234567', 'Bogotá, Colombia', '1990-05-15', 1, GETDATE());
+
+-- Insert Person 2
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Carlos Rodriguez', 'Masculino', '3112345678', 'Medellín, Colombia', '1985-08-20', 1, GETDATE());
+
+-- Insert Person 3
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Laura Gutierrez', 'Femenino', '3153456789', 'Cali, Colombia', '1993-12-10', 1, GETDATE());
+
+-- Insert Person 4
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Juan Perez', 'Masculino', '3164567890', 'Cartagena, Colombia', '1988-03-25', 1, GETDATE());
+
+-- Insert Person 5
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Sofia Ramirez', 'Femenino', '3175678901', 'Barranquilla, Colombia', '1995-07-05', 1, GETDATE());
+
+-- Insert Person 6
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Pedro Gomez', 'Masculino', '3186789012', 'Manizales, Colombia', '1980-09-18', 1, GETDATE());
+
+-- Insert Person 7
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Valentina Sanchez', 'Femenino', '3197890123', 'Pereira, Colombia', '1998-01-30', 1, GETDATE());
+
+-- Insert Person 8
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Andres Torres', 'Masculino', '3208901234', 'Santa Marta, Colombia', '1983-11-08', 1, GETDATE());
+
+-- Insert Person 9
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Camila Diaz', 'Femenino', '3219012345', 'Villavicencio, Colombia', '1992-04-12', 1, GETDATE());
+
+-- Insert Person 10
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Alejandro Jimenez', 'Masculino', '3220123456', 'Cúcuta, Colombia', '1987-06-28', 1, GETDATE());
+
+-- Insert Person 11
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Maria Hernandez', 'Femenino', '3231234567', 'Pasto, Colombia', '1994-09-22', 1, GETDATE());
+
+-- Insert Person 12
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Javier Rojas', 'Masculino', '3242345678', 'Tunja, Colombia', '1986-02-14', 1, GETDATE());
+
+-- Insert Person 13
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Daniela Ortiz', 'Femenino', '3253456789', 'Neiva, Colombia', '1991-07-30', 1, GETDATE());
+
+-- Insert Person 14
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Luis Morales', 'Masculino', '3264567890', 'Ibagué, Colombia', '1989-11-05', 1, GETDATE());
+
+-- Insert Person 15
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Valeria Castro', 'Femenino', '3275678901', 'Armenia, Colombia', '1997-04-18', 1, GETDATE());
+
+-- Insert Person 16
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Mateo Suarez', 'Masculino', '3286789012', 'Montería, Colombia', '1982-10-11', 1, GETDATE());
+
+-- Insert Person 17
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Isabella Garcia', 'Femenino', '3297890123', 'Popayán, Colombia', '1996-01-25', 1, GETDATE());
+
+-- Insert Person 18
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Ricardo Vargas', 'Masculino', '3308901234', 'Sincelejo, Colombia', '1984-08-08', 1, GETDATE());
+
+-- Insert Person 19
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Gabriela Rios', 'Femenino', '3319012345', 'Quibdó, Colombia', '1993-03-15', 1, GETDATE());
+
+-- Insert Person 20
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Sebastian Herrera', 'Masculino', '3320123456', 'Riohacha, Colombia', '1981-05-29', 1, GETDATE());
+
+-- Insert Person 21
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Juliana Medina', 'Femenino', '3331234567', 'Ciénaga, Colombia', '1994-08-12', 1, GETDATE());
+
+-- Insert Person 22
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Diego Ortega', 'Masculino', '3342345678', 'Tuluá, Colombia', '1986-01-24', 1, GETDATE());
+
+-- Insert Person 23
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Fabiola Ramirez', 'Femenino', '3353456789', 'Girardot, Colombia', '1991-06-29', 1, GETDATE());
+
+-- Insert Person 24
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Hector Soto', 'Masculino', '3364567890', 'Palmira, Colombia', '1989-10-04', 1, GETDATE());
+
+-- Insert Person 25
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Adriana Ospina', 'Femenino', '3375678901', 'Rionegro, Colombia', '1997-03-17', 1, GETDATE());
+
+-- Insert Person 26
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Mauricio Duque', 'Masculino', '3386789012', 'Magangué, Colombia', '1983-09-28', 1, GETDATE());
+
+-- Insert Person 27
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Isabel Velez', 'Femenino', '3397890123', 'Florencia, Colombia', '1996-02-11', 1, GETDATE());
+
+-- Insert Person 28
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Raul Molina', 'Masculino', '3408901234', 'Yopal, Colombia', '1984-07-07', 1, GETDATE());
+
+-- Insert Person 29
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Lina Herrera', 'Femenino', '3419012345', 'Tumaco, Colombia', '1993-02-22', 1, GETDATE());
+
+-- Insert Person 30
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Andres Salazar', 'Masculino', '3420123456', 'San Andrés, Colombia', '1981-04-05', 1, GETDATE());
+
+-- Insert Person 31
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Marcela Garcia', 'Femenino', '3431234567', 'Fusagasugá, Colombia', '1994-07-18', 1, GETDATE());
+
+-- Insert Person 32
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Felipe Mendoza', 'Masculino', '3442345678', 'Montería, Colombia', '1986-12-30', 1, GETDATE());
+
+-- Insert Person 33
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Sara Jimenez', 'Femenino', '3453456789', 'Riohacha, Colombia', '1991-05-15', 1, GETDATE());
+
+-- Insert Person 34
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Carlos Alvarez', 'Masculino', '3464567890', 'Cúcuta, Colombia', '1989-10-28', 1, GETDATE());
+
+-- Insert Person 35
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Laura Montoya', 'Femenino', '3475678901', 'Armenia, Colombia', '1997-02-09', 1, GETDATE());
+
+-- Insert Person 36
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Camilo Sánchez', 'Masculino', '3486789012', 'Santa Marta, Colombia', '1983-08-22', 1, GETDATE());
+
+-- Insert Person 37
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Valentina Morales', 'Femenino', '3497890123', 'Popayán, Colombia', '1996-01-05', 1, GETDATE());
+
+-- Insert Person 38
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Daniel Ríos', 'Masculino', '3508901234', 'Sincelejo, Colombia', '1984-06-18', 1, GETDATE());
+
+-- Insert Person 39
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Isabella Gómez', 'Femenino', '3519012345', 'Quibdó, Colombia', '1993-01-30', 1, GETDATE());
+
+-- Insert Person 40
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Juan Torres', 'Masculino', '3520123456', 'Tunja, Colombia', '1981-04-12', 1, GETDATE());
+
+-- Insert Person 41
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Luisa Ortega', 'Femenino', '3531234567', 'Valledupar, Colombia', '1994-09-22', 1, GETDATE());
+
+-- Insert Person 42
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Andrés Duque', 'Masculino', '3542345678', 'Palmira, Colombia', '1986-02-14', 1, GETDATE());
+
+-- Insert Person 43
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Fernanda Rodriguez', 'Femenino', '3553456789', 'Riohacha, Colombia', '1991-07-30', 1, GETDATE());
+
+-- Insert Person 44
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Carlos Soto', 'Masculino', '3564567890', 'Armenia, Colombia', '1989-11-05', 1, GETDATE());
+
+-- Insert Person 45
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Laura Ospina', 'Femenino', '3575678901', 'Barrancabermeja, Colombia', '1997-04-18', 1, GETDATE());
+
+-- Insert Person 46
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Juan Morales', 'Masculino', '3586789012', 'Buenaventura, Colombia', '1982-10-11', 1, GETDATE());
+
+-- Insert Person 47
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Valentina Suarez', 'Femenino', '3597890123', 'Ipiales, Colombia', '1996-01-25', 1, GETDATE());
+
+-- Insert Person 48
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Ricardo Vargas', 'Masculino', '3608901234', 'Sincelejo, Colombia', '1984-08-08', 1, GETDATE());
+
+-- Insert Person 49
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Gabriela Rios', 'Femenino', '3619012345', 'Arauca, Colombia', '1993-03-15', 1, GETDATE());
+
+-- Insert Person 50
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Sebastian Herrera', 'Masculino', '3620123456', 'Santa Marta, Colombia', '1981-05-29', 1, GETDATE());
+
+-- Insert Person 51
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('María Fernanda Medina', 'Femenino', '3631234567', 'Ciénaga, Colombia', '1994-08-12', 1, GETDATE());
+
+-- Insert Person 52
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Alejandro Torres', 'Masculino', '3642345678', 'Tuluá, Colombia', '1986-01-24', 1, GETDATE());
+
+-- Insert Person 53
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Valeria Ríos', 'Femenino', '3653456789', 'Girardot, Colombia', '1991-06-29', 1, GETDATE());
+
+-- Insert Person 54
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Juan Carlos Soto', 'Masculino', '3664567890', 'Palmira, Colombia', '1989-10-04', 1, GETDATE());
+
+-- Insert Person 55
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Laura Adría Ospina', 'Femenino', '3675678901', 'Rionegro, Colombia', '1997-03-17', 1, GETDATE());
+
+-- Insert Person 56
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Camilo Antonio Sánchez', 'Masculino', '3686789012', 'Magangué, Colombia', '1983-09-28', 1, GETDATE());
+
+-- Insert Person 57
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Isabel Cristina Velez', 'Femenino', '3697890123', 'Florencia, Colombia', '1996-02-11', 1, GETDATE());
+
+-- Insert Person 58
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Ricardo Andrés Molina', 'Masculino', '3708901234', 'Yopal, Colombia', '1984-07-07', 1, GETDATE());
+
+-- Insert Person 59
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Lina Paola Herrera', 'Femenino', '3719012345', 'Tumaco, Colombia', '1993-02-22', 1, GETDATE());
+
+-- Insert Person 60
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Andres Salazar', 'Masculino', '3720123456', 'San Andrés, Colombia', '1981-04-05', 1, GETDATE());
+
+-- Insert Person 61
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Marcela Andrea Garcia', 'Femenino', '3731234567', 'Fusagasugá, Colombia', '1994-07-18', 1, GETDATE());
+
+-- Insert Person 62
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Felipe Alejandro Mendoza', 'Masculino', '3742345678', 'Montería, Colombia', '1986-12-30', 1, GETDATE());
+
+-- Insert Person 63
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Sara Valentina Jimenez', 'Femenino', '3753456789', 'Riohacha, Colombia', '1991-05-15', 1, GETDATE());
+
+-- Insert Person 64
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Carlos Alberto Soto', 'Masculino', '3764567890', 'Armenia, Colombia', '1989-11-05', 1, GETDATE());
+
+-- Insert Person 65
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Laura Vanessa Ospina', 'Femenino', '3775678901', 'Barrancabermeja, Colombia', '1997-04-18', 1, GETDATE());
+
+-- Insert Person 66
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Juan David Morales', 'Masculino', '3786789012', 'Buenaventura, Colombia', '1982-10-11', 1, GETDATE());
+
+-- Insert Person 67
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Valentina Isabel Suarez', 'Femenino', '3797890123', 'Ipiales, Colombia', '1996-01-25', 1, GETDATE());
+
+-- Insert Person 68
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Ricardo Alonso Vargas', 'Masculino', '3808901234', 'Sincelejo, Colombia', '1984-08-08', 1, GETDATE());
+
+-- Insert Person 69
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Gabriela Alejandra Rios', 'Femenino', '3819012345', 'Arauca, Colombia', '1993-03-15', 1, GETDATE());
+
+-- Insert Person 70
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Sebastian Alejandro Herrera', 'Masculino', '3820123456', 'Santa Marta, Colombia', '1981-05-29', 1, GETDATE());
+
+-- Insert Person 71
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Isabella Sofia Velez', 'Femenino', '3831234567', 'Ciénaga, Colombia', '1994-08-12', 1, GETDATE());
+
+-- Insert Person 72
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Mateo Alejandro Medina', 'Masculino', '3842345678', 'Tuluá, Colombia', '1986-01-24', 1, GETDATE());
+
+-- Insert Person 73
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Valentina Andrea Ríos', 'Femenino', '3853456789', 'Girardot, Colombia', '1991-06-29', 1, GETDATE());
+
+-- Insert Person 74
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Juan Esteban Soto', 'Masculino', '3864567890', 'Palmira, Colombia', '1989-10-04', 1, GETDATE());
+
+-- Insert Person 75
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Laura Isabel Ospina', 'Femenino', '3875678901', 'Rionegro, Colombia', '1997-03-17', 1, GETDATE());
+
+-- Insert Person 76
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Camilo Andrés Sánchez', 'Masculino', '3886789012', 'Magangué, Colombia', '1983-09-28', 1, GETDATE());
+
+-- Insert Person 77
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Isabel Cristina Velez', 'Femenino', '3897890123', 'Florencia, Colombia', '1996-02-11', 1, GETDATE());
+
+-- Insert Person 78
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Ricardo Andrés Molina', 'Masculino', '3908901234', 'Yopal, Colombia', '1984-07-07', 1, GETDATE());
+
+-- Insert Person 79
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Lina Paola Herrera', 'Femenino', '3919012345', 'Tumaco, Colombia', '1993-02-22', 1, GETDATE());
+
+-- Insert Person 80
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Andres Salazar', 'Masculino', '3920123456', 'San Andrés, Colombia', '1981-04-05', 1, GETDATE());
+
+-- Insert Person 81
+INSERT INTO dbo.Person (FullName, Gender, NumberPhone, Address, BirthDate, UserId, CreationDate)
+VALUES ('Juliana Medina', 'Femenino', '3931234567', 'Ciénaga, Colombia', '1994-08-12', 1, GETDATE());
+
